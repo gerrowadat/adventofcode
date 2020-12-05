914a4456fdf836f7c6cabe73119c0b12ec3df512
Dave O'Connor <git@mail.andvari.net>
2020-12-05 13:39:37 +0000

    2015 4-* solution

---
+++ 2015/4/4-2.py
@@ -0,0 +1,15 @@
+import hashlib
+
+inp = 'ckczppom'
+
+num = 1
+
+while True:
+  h = hashlib.md5()
+  plain = '%s%d' % (inp, num)
+  h.update(plain)
+  if str(h.hexdigest()).startswith('000000'):
+    print('Lowest: %d' % (num, ))
+    break
+  num += 1
+
