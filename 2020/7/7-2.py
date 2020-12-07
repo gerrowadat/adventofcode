a2a0f15adabffac9e121ffcf62018c489aa70240
Dave O'Connor <git@mail.andvari.net>
2020-12-07 10:32:32 +0000

    7-2 solution

---
+++ 2020/7/7-2.py
@@ -0,0 +1,34 @@
+import re
+import sys
+
+def parserule(rule):
+  """Return (outer: (inner: inner_count) ...))"""
+  rule = rule.strip('.')
+  rule = rule.strip(',')
+  (outer, inner) = rule.split(' contain ')
+  # This is always ' bags'
+  outer = outer[0:-5]
+  # sigh....fine.
+  colourcounts = {}
+  for group in re.findall(r'(\d+)\s(.+?\s.+?)\b', inner):
+    colourcounts[group[1]] = int(group[0])
+  return (outer, colourcounts)
+
+def bag_count(rules, colour):
+  if len(rules[colour]) == 0:
+    return 1
+  count = 1 # This bag itself.
+  for bagcolour in rules[colour]:
+    count += (rules[colour][bagcolour] * bag_count(rules, bagcolour))
+  print('a %s bag is actually %d bags' % (colour, count))
+  return count
+    
+if len(sys.argv) > 1:
+  print parserule(sys.argv[1])
+
+with open("input.txt") as f:
+  lines = f.readlines()
+
+rules = dict([parserule(x.strip('\n')) for x in lines])
+
+print('A shiny gold bag (including itself) comprises %d bags' % (bag_count(rules, "shiny gold")))
