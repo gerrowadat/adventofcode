761847d3e72851e00961cd8ca28c0fab914a2a3d
Dave O'Connor <git@mail.andvari.net>
2020-12-11 15:48:51 +0000

    more cleanup

--- 2020/11/11-2.py
+++ 2020/11/11-2.py
@@ -1,4 +1,4 @@
-def diag(plan, i, j, i_step, j_step):
+def look(plan, i, j, i_step, j_step):
   here_i = i
   here_j = j
   while (0 <= here_i < len(plan)) and (0 <= here_j < len(plan[0])):
@@ -10,94 +10,49 @@ def diag(plan, i, j, i_step, j_step):
   return []
 
 def seen_seats(plan, i, j):
-  seen = []
+  all_seen = []
 
-  w = diag(plan, i, j, 0, -1)
-  if w:
-    seen.append(w)
+  for x in (-1, 0, 1):
+    for y in (-1, 0, 1):
+      if x == 0 and y == 0:
+        continue
+      seen = look(plan, i, j, x, y)
+      if seen:
+        all_seen.append(seen)
 
-  e = diag(plan, i, j, 0, 1)
-  if e:
-    seen.append(e)
-
-  n = diag(plan, i, j, -1, 0)
-  if n:
-    seen.append(n)
-
-  s = diag(plan, i, j, 1, 0)
-  if s:
-    seen.append(s)
-
-  ne = diag(plan, i, j, 1, 1)
-  if ne:
-    seen.append(ne)
-
-  nw = diag(plan, i, j, -1, 1)
-  if nw:
-    seen.append(nw)
-
-  se = diag(plan, i, j, 1, -1)
-  if se:
-    seen.append(se)
-
-  sw = diag(plan, i, j, -1, -1)
-  if sw:
-    seen.append(sw)
-
-  return seen
-
-def adjacent_occupied_seats(plan, i, j):
-  seats = seen_seats(plan, i, j)
-
-  adj = []
-  for s in seats:
-    if s != (i, j):
-      if s[0] >= 0 and s[1] >= 0:
-        if s[0] < len(plan) and s[1] < len(plan[0]):
-          adj.append(s)
-  occ = []
-  for s in adj:
-      if plan[s[0]][s[1]] == '#':
-        occ.append(s)
-  return occ
+  return all_seen
 
 def get_changes(plan):
   changes = []
+
   for i in range(0, len(plan)):
     for j in range(0, len(plan[i])):
-      if plan[i][j] == 'L':
-        adj = len(adjacent_occupied_seats(plan, i, j))
-        if adj == 0:
-          changes.append((i, j, '#',))
-      if plan[i][j] == '#':
-        adj = len(adjacent_occupied_seats(plan, i, j))
-        if adj >= 5:
-          changes.append((i, j, 'L',))
-  return changes
+      if plan[i][j] in ('L', '#'):
+        occupied = len([x for x in seen_seats(plan, i, j) if plan[x[0]][x[1]] == '#'])
+        if plan[i][j] == 'L':
+          if occupied == 0:
+            changes.append((i, j, '#',))
+        if plan[i][j] == '#':
+          if occupied >= 5:
+            changes.append((i, j, 'L',))
 
-def printplan(plan):
-    for row in plan:
-      print ''.join(row)
+  return changes
 
 def main():
   with open('input.txt') as f:
     rows = [x.strip('\n') for x in f.readlines()]
   plan = [list(x) for x in rows]
 
-  done = False
-
-  while not done:
+  while True:
     changes = get_changes(plan)
     if not changes:
       break
     for c in changes:
       plan[c[0]][c[1]] = c[2]
 
-  occupied = 0
-  for row in plan:
-    occupied += len([x for x in row if x == '#'])
+  occupied = sum([len([y for y in plan[x] if y == '#']) for x in range(0, len(plan))])
 
-  printplan(plan)
+  print('%s' % '\n'.join([''.join(x) for x in plan]))
 
   print('Occupied seats: %d' % (occupied, ))
 
