551646862fbb0518d8acf6b951348877ba4dc1aa
Dave O'Connor <git@mail.andvari.net>
2021-06-08 00:23:26 +0100

    10-1 solution

---
+++ 2015/10/10-1.py
@@ -0,0 +1,36 @@
+import sys
+
+def say(inp):
+    if len(inp) == 1:
+        return '1' + inp
+
+    out = ''
+    c = 1
+    curr = inp[0]
+    curr_count = 1
+    while c < len(inp):
+        if inp[c] == curr:
+            curr_count += 1
+            c += 1
+        elif inp[c] != curr:
+            out += '%s%s' % (curr_count, curr)
+            curr  = inp[c]
+            curr_count = 1
+            c += 1
+
+
+    out += '%s%s' % (curr_count, curr)
+
+    return out
+
+
+def main():
+    inp = sys.argv[1]
+    for i in range(40):
+        inp = say(inp)
+
+    print(len(inp))
+
+
+if __name__ == '__main__':
+    main()
