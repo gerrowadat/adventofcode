74e135bdb972f0fb1dcf10188c1cb0975db1eb90
Dave O'Connor <daveoc+git@elastic.co>
2021-11-30 00:12:00 +0000

    2016 Day 6 Part 2.

---
+++ 2016/6/2.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+
+
+def main():
+    with open('input.txt') as f:
+        lines = [x.strip() for x in f.readlines()]
+
+    msg = ''
+
+    for col in range(len(lines[0])):
+        freqs = {}
+        for ch in [x[col] for x in lines]:
+            freqs[ch] = freqs.get(ch, 0) + 1
+        msg += sorted(freqs.keys(), key=lambda x: freqs[x])[0]
+
+    print(msg)
+
+if __name__ == '__main__':
+    main()
