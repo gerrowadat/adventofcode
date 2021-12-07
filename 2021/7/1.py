1b749a6328f1d350868944de2577b056ce2e6a5d
Dave O'Connor <daveoc+git@elastic.co>
2021-12-07 13:08:08 +0000

    Be a oneliner smartypants before looking at Part 2.

--- 2021/7/1.py
+++ 2021/7/1.py
@@ -1,13 +1,10 @@
 #!/usr/bin/env python3
 
-def fuel_to_pos(pos, crabs):
-    return sum([abs(x - pos) for x in crabs])
-
 def main():
     with open('input.txt') as f:
         crabs = [int(x) for x in f.read().strip().split(',')]
 
-    print('Min fuel: %d' % (min([fuel_to_pos(x, crabs) for x in range(min(crabs), max(crabs)+1)])))
+    print('Min fuel: %d' % (min([sum([abs(y - x) for y in crabs]) for x in range(min(crabs), max(crabs)+1)])))
 
 if __name__ == '__main__':
     main()
