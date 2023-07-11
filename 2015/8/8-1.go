95324644c125fade01c095186c695ed0271b7b3c
Dave O'Connor <git@mail.andvari.net>
2023-07-11 21:46:32 +0100

    2015 8-1 in go.

---
+++ 2015/8/8-1.go
@@ -0,0 +1,53 @@
+// Doesn't work (yet).
+package main
+
+import (
+    "fmt"
+)
+
+func getValueLength(raw string) (int) {
+    c := 0
+    ret := 0
+    for c < len(raw) {
+        if len(raw) - c == 1 {
+            c += 1
+            continue
+        }
+        if raw[c:c+2] == "\\\"" {
+            ret += 1
+            c += 2
+            continue
+        } 
+        if raw[c:c+2] == "\\\\" {
+            ret += 1
+            c += 2
+            continue
+        }
+        if raw[c:c+2] == "\\x" {
+            ret += 1
+            c += 4
+            continue
+        }
+        if string(raw[c]) != "\"" {
+            ret += 1
+        }
+        c += 1
+    }
+    return ret
+}
+
+func main() {
+    unprocessed := getFileLines("input.txt")
+
+    code_len := 0
+    value_len := 0
+
+    for _, l := range unprocessed {
+        code_len += len(l)
+        this_val_len := getValueLength(l)
+        fmt.Printf("%s: %d -> %d\n", l, len(l), this_val_len)
+        value_len += this_val_len
+    }
+
+    fmt.Printf("Difference: %d\n", code_len - value_len)
+}
