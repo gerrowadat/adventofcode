3fd7b59efc88273d9cb2fd19daa6176992d08af0
Dave O'Connor <git@mail.andvari.net>
2023-12-03 07:08:53 +0000

    Day 3 part 2

--- 2023/03/main.go
+++ 2023/03/main.go
@@ -86,6 +86,41 @@ func symbolAdjacent(grid []string, n *Num) bool {
 	return false
 }
 
+func findAllInGrid(grid []string, want byte) []*Point {
+	ret := []*Point{}
+	for y := range grid {
+		for x := range grid[y] {
+			if grid[y][x] == want {
+				ret = append(ret, &Point{x: x, y: y})
+			}
+		}
+	}
+	return ret
+}
+
+func appendUniqueNum(current []*Num, new *Num) []*Num {
+	for _, n := range current {
+		if n.loc.x == new.loc.x && n.loc.y == new.loc.y {
+			return current
+		}
+	}
+	return append(current, new)
+}
+
+func adjacentNums(all_nums []*Num, p *Point) []*Num {
+	ret := []*Num{}
+	for _, n := range all_nums {
+		for x := p.x - 1; x <= p.x+1; x++ {
+			for y := p.y - 1; y <= p.y+1; y++ {
+				if isInsideNum(n, x, y) {
+					ret = appendUniqueNum(ret, n)
+				}
+			}
+		}
+	}
+	return ret
+}
+
 func main() {
 	grid := getFileLines("input.txt")
 
@@ -99,4 +134,14 @@ func main() {
 		}
 	}
 	fmt.Printf("Part 1: %d\n", sum)
+
+	all_ratios := 0
+	maybe_gears := findAllInGrid(grid, '*')
+	for _, g := range maybe_gears {
+		nums := adjacentNums(all_nums, g)
+		if len(nums) == 2 {
+			all_ratios = all_ratios + (nums[0].val * nums[1].val)
+		}
+	}
+	fmt.Printf("Part 2: %d\n", all_ratios)
 }
