f397d8db8af450dc9df33b0dfc07adb233c44c9d
Dave O'Connor <git@mail.andvari.net>
2024-11-30 19:44:58 +0000

    20917 day 7 part 2.

--- 2017/7/main.go
+++ 2017/7/main.go
@@ -18,9 +18,10 @@ func SplitCommaSeparated(in string) []string {
 }
 
 type Program struct {
-	Name     string
-	Weight   int
-	Children []string
+	Name        string
+	Weight      int
+	TotalWeight int
+	Children    []string
 }
 
 func (p *Program) FromString(s string) error {
@@ -56,6 +57,15 @@ type Tower struct {
 
 func (t *Tower) AddProgram(p Program) {
 	t.Programs = append(t.Programs, p)
+}
+
+func (t *Tower) GetProgram(name string) Program {
+	for _, p := range t.Programs {
+		if p.Name == name {
+			return p
+		}
+	}
+	return Program{}
 }
 
 func (t *Tower) GetProgramTotalWeight(name string) int {
@@ -96,8 +106,69 @@ func (t *Tower) GetRootProgram() Program {
 	return Program{}
 }
 
+func (t *Tower) FindUnbalancedProgram() Program {
+	// Find the program that has children with different weights.
+	for _, p := range t.Programs {
+		if len(p.Children) > 0 {
+			weights := []int{}
+			for _, c := range p.Children {
+				weights = append(weights, t.GetProgramTotalWeight(c))
+			}
+			// Check if all the weights are the same.
+			weightcounts := map[int]int{}
+			for _, w := range weights {
+				weightcounts[w]++
+			}
+			if len(weightcounts) > 1 {
+				// Find the program that has the wrong weight.
+				// Find the weight that is unique, and find the program that has that weight.
+				uniqueWeight := 0
+				uniqueProgram := Program{}
+				for w, _ := range weightcounts {
+					if weightcounts[w] == 1 {
+						uniqueWeight = w
+					}
+					break
+				}
+				for _, c := range p.Children {
+					if t.GetProgramTotalWeight(c) == uniqueWeight {
+						uniqueProgram = t.GetProgram(c)
+						break
+					}
+				}
+				return uniqueProgram
+			}
+		}
+	}
+	return Program{}
+}
+
+func (t *Tower) GetExpectedProgramTotalWeight(p Program) int {
+	// Find the total weight that the given program should have.
+	for _, o := range t.Programs {
+		for i, c := range o.Children {
+			if c == p.Name {
+				// Return the weight of one of the program's siblings.
+				return t.GetProgramTotalWeight(o.Children[(i+1)%len(o.Children)])
+			}
+		}
+	}
+	return 0
+}
+
+func (t *Tower) GetParentProgram(name string) Program {
+	// Find the program that has the given program as a child.
+	for _, p := range t.Programs {
+		for _, c := range p.Children {
+			if c == name {
+				return p
+			}
+		}
+	}
+	return Program{}
+}
+
 func main() {
-	fmt.Println("Hello, World!")
 	lines, err := aocutil.GetFileLines("input.txt")
 	if err != nil {
 		fmt.Println(err)
@@ -121,5 +192,10 @@ func main() {
 	fmt.Println("Part 1: ", t.GetRootProgram().Name)
 
 	// Part 2: Find the one unbalanced program and see what it needs to weigh.
+	// Bonus points for avoiding building an actual tree structure at all costs.
+	// Extra bonus points because there's a race(?) in here somewhere which means I get an answer only sometimes. Quality.
+	unbalanced := t.FindUnbalancedProgram()
+	ideal := t.GetExpectedProgramTotalWeight(unbalanced) - (t.GetProgramTotalWeight(unbalanced.Name) - unbalanced.Weight)
+	fmt.Printf("Part 2: %s should weigh %d\n", unbalanced.Name, ideal)
 
 }
