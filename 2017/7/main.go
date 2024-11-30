4e50eafa1d4a8b0bca681f25e4b3466f8bb17aa6
Dave O'Connor <git@mail.andvari.net>
2024-11-30 18:54:23 +0000

    Clean this up a bit, add tower struct.

--- 2017/7/main.go
+++ 2017/7/main.go
@@ -8,12 +8,6 @@ import (
 	"github.com/gerrowadat/adventofcode/aocutil"
 )
 
-type Program struct {
-	Name     string
-	Weight   int
-	Children []string
-}
-
 func SplitCommaSeparated(in string) []string {
 	ret := []string{}
 	raw := strings.Split(in, ",")
@@ -21,6 +15,12 @@ func SplitCommaSeparated(in string) []string {
 		ret = append(ret, strings.TrimSpace(r))
 	}
 	return ret
+}
+
+type Program struct {
+	Name     string
+	Weight   int
+	Children []string
 }
 
 func (p *Program) FromString(s string) error {
@@ -50,30 +50,34 @@ func (p *Program) String() string {
 	return fmt.Sprintf("%s (%d) -> %v", p.Name, p.Weight, p.Children)
 }
 
-func main() {
-	fmt.Println("Hello, World!")
-	lines, err := aocutil.GetFileLines("input.txt")
-	if err != nil {
-		fmt.Println(err)
-		return
-	}
+type Tower struct {
+	Programs []Program
+}
 
-	all_programs := []Program{}
-	for _, line := range lines {
-		p := Program{}
-		err := p.FromString(line)
-		if err != nil {
-			fmt.Println(err)
-			return
+func (t *Tower) AddProgram(p Program) {
+	t.Programs = append(t.Programs, p)
+}
+
+func (t *Tower) GetProgramTotalWeight(name string) int {
+	// Find the program with the given name, then get the total weight of that program and its children.
+	for _, p := range t.Programs {
+		if p.Name == name {
+			total := p.Weight
+			for _, c := range p.Children {
+				total += t.GetProgramTotalWeight(c)
+			}
+			return total
 		}
-		all_programs = append(all_programs, p)
 	}
-	// Part 1 : find the program with children that doesn't feature in any other program's children.
-	// This is some T7-9 tree traversal shit.
-	for _, p := range all_programs {
+	return 0
+}
+
+func (t *Tower) GetRootProgram() Program {
+	// Find the program that doesn't feature in any other program's children.
+	for _, p := range t.Programs {
 		if len(p.Children) > 0 {
 			found := false
-			for _, o := range all_programs {
+			for _, o := range t.Programs {
 				for _, c := range o.Children {
 					if c == p.Name {
 						found = true
@@ -85,9 +89,37 @@ func main() {
 				}
 			}
 			if !found {
-				fmt.Println("Part 1: ", p.Name)
-				break
+				return p
 			}
 		}
 	}
+	return Program{}
+}
+
+func main() {
+	fmt.Println("Hello, World!")
+	lines, err := aocutil.GetFileLines("input.txt")
+	if err != nil {
+		fmt.Println(err)
+		return
+	}
+
+	t := Tower{}
+
+	for _, line := range lines {
+		p := Program{}
+		err := p.FromString(line)
+		if err != nil {
+			fmt.Println(err)
+			return
+		}
+		t.AddProgram(p)
+	}
+
+	// Part 1 : find the program with children that doesn't feature in any other program's children.
+	// This is some T7-9 tree traversal shit.
+	fmt.Println("Part 1: ", t.GetRootProgram().Name)
+
+	// Part 2: Find the one unbalanced program and see what it needs to weigh.
+
 }
