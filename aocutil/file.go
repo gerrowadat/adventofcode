517e72a179e116a59e182d3ea4304cf881f3a765
Dave O'Connor <git@mail.andvari.net>
2024-12-02 10:39:42 +0000

    Promote GetIntMatrixFromFile to here.

--- aocutil/file.go
+++ aocutil/file.go
@@ -14,7 +14,7 @@ func GetFileNumber(filename string) (ret int, err error) {
 	}
 
 	if len(content) != 1 {
-		return 0, fmt.Errorf("File %v does not contain a single line", filename)
+		return 0, fmt.Errorf("file %v does not contain a single line", filename)
 	}
 
 	return strconv.Atoi(content[0])
@@ -23,7 +23,7 @@ func GetFileNumber(filename string) (ret int, err error) {
 func GetFileLines(filename string) (ret []string, err error) {
 	var content []byte
 	if content, err = os.ReadFile(filename); err != nil {
-		return nil, fmt.Errorf("Error reading %v : %v\n", filename, err)
+		return nil, fmt.Errorf("error reading %v : %v", filename, err)
 	}
 
 	return strings.Split(string(content), "\n"), nil
@@ -39,8 +39,32 @@ func GetFileLinesAsInts(fileName string) ([]int, error) {
 	for i, line := range lines {
 		ints[i], err = strconv.Atoi(line)
 		if err != nil {
-			return nil, fmt.Errorf("Non-int on line %d: %v (%v)", i, line, err)
+			return nil, fmt.Errorf("non-int on line %d: %v (%v)", i, line, err)
 		}
 	}
 	return ints, nil
+}
+
+func GetIntMatrixFromFile(fn, sep string) ([][]int, error) {
+	ret := [][]int{}
+
+	lines, err := GetFileLines(fn)
+	if err != nil {
+		fmt.Println("error reading file: ", err)
+		os.Exit(1)
+	}
+
+	for _, l := range lines {
+		fragments := strings.Split(l, sep)
+		row := []int{}
+		for _, f := range fragments {
+			elem, err := strconv.Atoi(f)
+			if err != nil {
+				return nil, err
+			}
+			row = append(row, elem)
+		}
+		ret = append(ret, row)
+	}
+	return ret, nil
 }
