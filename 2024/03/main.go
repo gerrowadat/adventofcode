ace97d14d4aeddc4042645232b5232ece25c3c13
Dave O'Connor <git@mail.andvari.net>
2024-12-03 12:15:58 +0000

    2024 day 3 part 2

--- 2024/03/main.go
+++ 2024/03/main.go
@@ -2,15 +2,23 @@ package main
 
 import (
 	"fmt"
+	"os"
 	"strconv"
 	"strings"
-
-	"github.com/gerrowadat/adventofcode/aocutil"
 )
 
 type Mul struct {
 	l int
 	r int
+}
+
+func GetFileToString(filename string) (string, error) {
+	var content []byte
+	content, err := os.ReadFile(filename)
+	if err != nil {
+		return "", fmt.Errorf("error reading %v : %v", filename, err)
+	}
+	return string(content), nil
 }
 
 func GetOperands(e string) (int, int, error) {
@@ -52,28 +60,94 @@ func GetMulExpresions(line string) []Mul {
 	return ret
 }
 
+func ConsumeMulExpression(in string) Mul {
+	l, r, err := GetOperands(in)
+	if err != nil {
+		return Mul{}
+	}
+	return Mul{l, r}
+}
+
+func ConsumeExpressions(line string) <-chan Mul {
+	ch := make(chan Mul)
+	c := 0
+	go func(line string) {
+		do := true
+		defer close(ch)
+		for {
+			next_do := strings.Index(line[c:], "do()")
+			next_dont := strings.Index(line[c:], "don't()")
+			next_maybemul := strings.Index(line[c:], "mul(")
+			if next_do == -1 && next_dont == -1 && next_maybemul == -1 {
+				break
+			}
+			// Remove -1 from consideration
+			nexts := []int{}
+			if next_do != -1 {
+				nexts = append(nexts, next_do)
+			}
+			if next_dont != -1 {
+				nexts = append(nexts, next_dont)
+			}
+			if next_maybemul != -1 {
+				nexts = append(nexts, next_maybemul)
+			}
+			next := len(line)
+			for i := range nexts {
+				if nexts[i] > -1 && nexts[i] < next {
+					next = nexts[i]
+				}
+			}
+			switch next {
+			case next_do:
+				do = true
+				c += (next_do + 4)
+			case next_dont:
+				do = false
+				c += (next_dont + 7)
+			case next_maybemul:
+				if do {
+					mul := ConsumeMulExpression(line[c+next_maybemul+3:])
+					if mul.l != 0 && mul.r != 0 {
+						ch <- mul
+					}
+				}
+				c += (next_maybemul + 4)
+			default:
+				fmt.Println("Shouldn't be here.")
+			}
+		}
+	}(line)
+	return ch
+}
+
 func EvaluateMulExpression(e Mul) int {
 	return e.l * e.r
 }
 
 func main() {
 	fmt.Println("Hello.")
-	lines, err := aocutil.GetFileLines("input.txt")
+	lines, err := GetFileToString("input.txt")
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	muls := []Mul{}
-	for i := range lines {
-		muls = append(muls, GetMulExpresions(lines[i])...)
-	}
-
 	mul_sum := 0
+	muls := GetMulExpresions(lines)
 	for i := range muls {
 		mul_sum += EvaluateMulExpression(muls[i])
 	}
 
 	fmt.Println("Part 1: ", mul_sum)
+
+	cond_muls := ConsumeExpressions(lines)
+
+	mul_sum = 0
+	for m := range cond_muls {
+		mul_sum += EvaluateMulExpression(m)
+	}
+
+	fmt.Println("Part 2: ", mul_sum)
 
 }
