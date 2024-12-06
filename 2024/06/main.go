51a069eb611358a6f7bca2b166f38fe11897be9a
Dave O'Connor <git@mail.andvari.net>
2024-12-06 14:45:21 +0000

    2024 Day 6 part 2 (incomplete)

--- 2024/06/main.go
+++ 2024/06/main.go
@@ -19,27 +19,35 @@ type Guard struct {
 	facing Facing
 }
 
+type Turn struct {
+	pos        Point
+	new_facing Facing
+}
+
 type Map struct {
 	grid    [][]rune
 	g       Guard
 	visited []Point
+	turns   []Turn
 }
 
 func NewMap(r [][]rune) Map {
-	// copy the grid, erase the guard rune and move tha tinfo to the Guard struct
+	// copy the grid, populatethe Guard struct
 	var g Guard
+	grid := make([][]rune, len(r))
+
 	for i := 0; i < len(r); i++ {
+		grid[i] = append([]rune(nil), r[i]...)
 		for j := 0; j < len(r[0]); j++ {
 			if r[i][j] == '^' {
 				g.pos = Point{y: i, x: j}
 				// guards always face north (for now)
 				g.facing = 1
-				r[i][j] = '.'
 			}
 		}
 	}
 	visited := []Point{g.pos}
-	return Map{grid: r, g: g, visited: visited}
+	return Map{grid: grid, g: g, visited: visited, turns: []Turn{}}
 }
 
 func (m *Map) String() string {
@@ -100,16 +108,49 @@ func (m *Map) MoveGuard() {
 	}
 
 	// Last, switch facing if we're facing an obstacle.
-	if m.NextRune() == '#' {
+	if !m.GuardExiting() && m.NextRune() == '#' {
 		if m.g.facing == 4 {
 			m.g.facing = 1
 		} else {
 			m.g.facing++
 		}
+		// Store our facing
+		turn := Turn{pos: Point{y: m.g.pos.y, x: m.g.pos.x}, new_facing: m.g.facing}
+		m.turns = append(m.turns, turn)
+
 	}
 
 	// Mark our new location as visited
 	m.Visit(m.g.pos)
+}
+
+func (m *Map) Looping() bool {
+	// Find a past occurance of our most recent turn, and see if there's a loop since we were last here.
+	loopturns := []int{}
+	for i := 0; i < len(m.turns); i++ {
+		if m.turns[i] == m.turns[len(m.turns)-1] {
+			loopturns = append(loopturns, i)
+		}
+	}
+
+	if len(loopturns) < 3 {
+		return false
+	}
+
+	loop1 := m.turns[loopturns[len(loopturns)-2]:loopturns[len(loopturns)-1]]
+	loop2 := m.turns[loopturns[len(loopturns)-3]:loopturns[len(loopturns)-2]]
+
+	if len(loop1) != len(loop2) {
+		return false
+	}
+
+	for i := 0; i < len(loop1); i++ {
+		if loop1[i].pos.x != loop2[i].pos.x || loop1[i].pos.y != loop2[i].pos.y || loop1[i].new_facing != loop2[i].new_facing {
+			return false
+		}
+	}
+
+	return true
 }
 
 func (m *Map) GuardExiting() bool {
@@ -135,6 +176,41 @@ func (m *Map) GuardExiting() bool {
 	return false
 }
 
+func (m *Map) NewObstacleLoops(p Point, itercount int) bool {
+	m.grid[p.y][p.x] = '#'
+	for i := 0; i < itercount; i++ {
+		if m.GuardExiting() {
+			m.grid[p.y][p.x] = '.'
+			return false
+		}
+		m.MoveGuard()
+	}
+	m.grid[p.y][p.x] = '.'
+	return m.Looping()
+}
+
+func FindLoopingObstacles(r [][]rune) []Point {
+	// Post-note: It's more efficient to track duplicate turns as we go and halt when we hit a loop,
+	// but here we all are.
+	m := NewMap(r)
+	g := Guard{pos: Point{x: m.g.pos.x, y: m.g.pos.y}, facing: m.g.facing}
+
+	ret := []Point{}
+	for y := 0; y < len(m.grid); y++ {
+		for x := 0; x < len(m.grid[0]); x++ {
+			if m.grid[y][x] == '.' {
+				// Reset the guard, run 100 iterations, see if there's a loop.
+				m.g = g
+				// Fun note for future Dave, we seem to find fewer loops the more iterations we do, so there's some stupid bug somewhere and maybe someday I'll find it.
+				if m.NewObstacleLoops(Point{x: x, y: y}, 100) {
+					ret = append(ret, Point{x: x, y: y})
+				}
+			}
+		}
+	}
+	return ret
+}
+
 func main() {
 	fmt.Println("Hello.")
 	runes, err := aocutil.GetRuneMatrixFromFile("input.txt")
@@ -145,11 +221,14 @@ func main() {
 	m := NewMap(runes)
 
 	fmt.Printf("Grid Dimensions %dx%d, Guard found at [%d,%d] with facing %d\n", len(m.grid), len(m.grid[0]), m.g.pos.y, m.g.pos.x, m.g.facing)
-	//fmt.Println(m.String())
 
 	for !m.GuardExiting() {
 		m.MoveGuard()
 	}
 
 	fmt.Println("Part 1: ", len(m.visited))
+
+	loop_points := FindLoopingObstacles(runes)
+
+	fmt.Printf("Part 2: %d\n", len(loop_points))
 }
