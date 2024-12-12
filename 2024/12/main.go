7959d677c43cffe8f5648e748f68facb6ec60502
Dave O'Connor <git@mail.andvari.net>
2024-12-12 17:02:57 +0000

    2024 Day 12 Part 2.

--- 2024/12/main.go
+++ 2024/12/main.go
@@ -101,6 +101,14 @@ func (r *Region) Perimeter(g [][]rune) int {
 	return ret
 }
 
+func (r *Region) Edges(g [][]rune) int {
+	ret := 0
+	for i := range r.points {
+		ret += GetCornersAtPoint(g, i)
+	}
+	return ret
+}
+
 func GetContainingRegion(r []Region, p Point) *Region {
 	for i := range r {
 		if r[i].ContainsPoint(p) {
@@ -108,6 +116,80 @@ func GetContainingRegion(r []Region, p Point) *Region {
 		}
 	}
 	return nil
+}
+
+// Return either the rune at coords, or . if out of bounds.
+func GridFetch(g [][]rune, p Point) rune {
+	if p.y < 0 || p.x < 0 || p.y >= len(g) || p.x >= len(g[0]) {
+		return '.'
+	}
+	return g[p.y][p.x]
+}
+
+func GetCornersAtPoint(g [][]rune, p Point) int {
+	ret := 0
+	label := g[p.y][p.x]
+
+	// TODO: Find smarter mathsy way of doing thiHAHA JUST KIDDING
+	var p1, p2, p3 rune
+	// tl
+	p1 = GridFetch(g, Point{x: p.x - 1, y: p.y})
+	p2 = GridFetch(g, Point{x: p.x, y: p.y - 1})
+	p3 = GridFetch(g, Point{x: p.x - 1, y: p.y - 1})
+	if label != p1 && label != p2 {
+		// *.
+		// .#
+		ret++
+	}
+	if label == p1 && label == p2 && label != p3 {
+		// .#
+		// ##
+		ret++
+	}
+	// tr
+	p1 = GridFetch(g, Point{x: p.x, y: p.y - 1})
+	p2 = GridFetch(g, Point{x: p.x + 1, y: p.y})
+	p3 = GridFetch(g, Point{x: p.x + 1, y: p.y - 1})
+	if label != p1 && label != p2 {
+		// .*
+		// #.
+		ret++
+	}
+	if label == p1 && label == p2 && label != p3 {
+		// #.
+		// ##
+		ret++
+	}
+	// bl
+	p1 = GridFetch(g, Point{x: p.x - 1, y: p.y})
+	p2 = GridFetch(g, Point{x: p.x, y: p.y + 1})
+	p3 = GridFetch(g, Point{x: p.x - 1, y: p.y + 1})
+	if label != p1 && label != p2 {
+		// .*
+		// #.
+		ret++
+	}
+	if label == p1 && label == p2 && label != p3 {
+		// #.
+		// ##
+		ret++
+	}
+	// br
+	p1 = GridFetch(g, Point{x: p.x, y: p.y + 1})
+	p2 = GridFetch(g, Point{x: p.x + 1, y: p.y})
+	p3 = GridFetch(g, Point{x: p.x + 1, y: p.y + 1})
+	if label != p1 && label != p2 {
+		// .*
+		// #.
+		ret++
+	}
+	if label == p1 && label == p2 && label != p3 {
+		// #.
+		// ##
+		ret++
+	}
+	//fmt.Printf(" - Corners of [%d,%d]: %v\n", p.y, p.x, ret)
+	return ret
 }
 
 func main() {
@@ -126,7 +208,7 @@ func main() {
 			if GetContainingRegion(regions, here) == nil {
 				new_r := Region{label: grid[i][j], points: map[Point]bool{}, gridsize: Point{y: len(grid), x: len(grid[0])}}
 				new_r.FindPoints(grid, here)
-				//fmt.Printf("New Region %s of area %d, permieter %d: %v\n", string(new_r.label), new_r.Area(), new_r.Perimeter(grid), new_r.points)
+				//fmt.Printf("New Region %s of area %d, perimeter %d, %d corners: %v\n", string(new_r.label), new_r.Area(), new_r.Perimeter(grid), new_r.Edges(grid), new_r.points)
 				regions = append(regions, new_r)
 			}
 		}
@@ -136,13 +218,16 @@ func main() {
 
 	fencing_price := 0
 	totarea := 0
+	edgeprice := 0
 	for i := range regions {
 		fencing_price += regions[i].Area() * regions[i].Perimeter(grid)
+		edgeprice += regions[i].Area() * regions[i].Edges(grid)
 		totarea += regions[i].Area()
 	}
 
 	fmt.Printf("Total area of all regions: %d/%d\n", totarea, (len(grid) * len(grid[0])))
 
 	fmt.Println("Part 1: ", fencing_price)
+	fmt.Println("Part 1: ", edgeprice)
 
 }
